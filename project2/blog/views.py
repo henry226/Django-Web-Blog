a85b8626d76414a71b70390e7f2457ddae59c0b5
henry226 <henry5581984@gmail.com>
2018-09-17 00:37:09 -0400

    adding template

--- project2/blog/views.py
+++ project2/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Henry',
+        'title': 'Blog Post 1',
+        'content': 'Fist post content',
+        'date_posted': '8/27, 2018'
+    },
+    {
+        'author': 'Henry',
+        'title': 'Blog Post 1',
+        'content': 'Fist post content',
+        'date_posted': '8/27, 2018'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
